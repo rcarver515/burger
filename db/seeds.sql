502729f6c26ecfcc764d1f9f0e4622c97a6cec8d
Robert Grant <rcarver515@gmail.com>
2019-04-30 23:17:12 -0500

    Initial commit of completed instructions

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", true)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avacado Burger", true)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Nashvegas Hot Chicken Burger", true)
